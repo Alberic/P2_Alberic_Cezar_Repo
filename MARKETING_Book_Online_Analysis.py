657d4c29ae4d7421973adb488b085c93a9a6eb2c
Albéric <cezar.alberic@gmail.com>
2021-07-14 23:07:22 +0200

    Fonctions de récupération de la page et fonction des récupération des adresse des livres sur la page

--- MARKETING_Book_Online_Analysis.py
+++ MARKETING_Book_Online_Analysis.py
@@ -2,25 +2,23 @@ import requests
 from bs4 import BeautifulSoup
 import prompt
 
-def obtenir_url():
-    url = prompt.string("URL :")
-    assert url.startswith("http"), "L'url n'est pas valide"
-    return url
-
-
 def obtenir_page(url_page):
     page = requests.get(url_page)
     assert page.ok, "Impossible d'obtenir la page"
     return page
 
 
-def parser_page(page_a_parser):
-    texte_a_parser = page_a_parser.text
-    page_parse = BeautifulSoup(texte_a_parser, "html.parser")
-    return page_parse
+def adresse_livres(page_a_parser):
+    adresse_livre = []
+    page_parse = BeautifulSoup(page_a_parser.text, "html.parser")
+    livre = page_parse.findAll('h3')
+    for title in livre:
+        href = title.a['href']
+        adresse_livre.append(page_a_parser.url + href)
+    return adresse_livre
 
 
-test_url = obtenir_url()
-test_page = obtenir_page(test_url)
-test_parse = parser_page(test_page)
-print(test_parse.text)
+url = "https://books.toscrape.com/"
+page = obtenir_page(url)
+adresse = adresse_livres(page)
+print(adresse)
